7b634307ae82391b329b306f991b4e2a959c4463
sree <sreelu.ph@gmail.com>
2020-08-20 21:21:57 -0600

    Added loops

--- js/app.js
+++ js/app.js
@@ -1,20 +1,25 @@
-var age=17;
-var is_subscribed=false;
-
-if(age<18 && !is_subscribed) {
-   console.log(" the user is younger than 18 and not subscribed");
-}
-else if(age>=18 && !is_subscribed) {
-    console.log("the user is 18 and older and not subscribed");
-}
-else if(age<18 && is_subscribed) {
-    console.log("the user is younger than 18 and is subscribed");
-
-}
-else if(age>=18 && is_subscribed)
+var age=[20,17,16,34];
+var is_subscribed=[false,false,true,true];
+for(index=0;index<age.length;index++)
 {
-    console.log("the user is 18 and older and is subscribed");
+    console.log("user age is:", age[index]);
+    if(age[index]<18 && !is_subscribed[index]) {
+        
+        console.log(" the user is younger than 18 and not subscribed");
+    }
+    else if(age[index]>=18 && !is_subscribed[index]) {
+        console.log("the user is 18 and older and not subscribed");
+    }
+    else if(age[index]<18 && is_subscribed[index]) {
+        console.log("the user is younger than 18 and is subscribed");
+    }
+    else if(age[index]>=18 && is_subscribed[index])
+    {
+        console.log("the user is 18 and older and is subscribed");
+    }
+    else {
+        console.log("no one is subscribed");
+    }
 }
-else {
-    console.log("no one is subscribed");
-}
+    
+
